79af1387d40e441337192c932173e79fa18a5f19
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2021-07-19 08:25:53 -0500

    echo library

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func triple_argument() {
+	fmt.Println("ee")
+}
+
+func main() {
+	m := make(map[int][]string)
+
+	// m[200] = "alvaro"
+	// m[201] = "31"
+
+	fmt.Println(m)
+}
